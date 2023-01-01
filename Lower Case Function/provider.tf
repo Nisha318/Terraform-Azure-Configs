31bcd38572411739153b9e944444a040767f9685
Nisha Prudhomme <eunishap@gmail.com>
2023-01-01 13:52:11 -0600

    Add files via upload

---
+++ Lower Case Function/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.10.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = ""
+  tenant_id = ""
+  client_id = ""
+  client_secret = ""
+  features {}  
+}
